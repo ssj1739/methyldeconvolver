17fb64935c076eba370f45b24016252a5d8973fd
ssj36 <ssj36@tbio.bioinformatics.georgetown.edu>
2023-05-11 12:20:01 -0400

    Merging

--- test_scripts/test_megan_rad_ref.R
+++ test_scripts/test_megan_rad_ref.R
@@ -1,10 +1,10 @@
 # Learn Full Reference
 library(methyldeconvolveR)
 
-pat.dir <- "~/Documents/Research/Wellstein/Projects/Megan_Radiation/Methylomes/PAT/"
-#pat.dir <- "~/Data/TestMixIn/from_megan_rad/Reference/ALL/CopyPAT/final_PAT_reference_SJ_Jan2023/pat/"
-marker.file <- "~/Documents/Research/Wellstein/Projects/Megan_Radiation/Methylomes/Marker/markers.ALL.bed"
-#marker.file <- "~/Data/TestMixIn/from_megan_rad/Markers/FinalMarkers/markers.ALL.bed"
+# pat.dir <- "~/Documents/Research/Wellstein/Projects/Megan_Radiation/Methylomes/PAT/"
+pat.dir <- "~/Data/TestMixIn/from_megan_rad/Reference/ALL/CopyPAT/final_PAT_reference_SJ_Jan2023/pat/original_pats/"
+#marker.file <- "~/Documents/Research/Wellstein/Projects/Megan_Radiation/Methylomes/Marker/markers.ALL.bed"
+marker.file <- "~/Data/TestMixIn/from_megan_rad/Markers/FinalMarkers/markers.ALL.bed"
 
 marker <- read_marker(marker.file)
 pat.files = dir(pattern = "*.pat.gz$", pat.dir, full.names = F)
